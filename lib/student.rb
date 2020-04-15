09e83a7df345b3da1054b3d1795f00bed5992ef1
Elijah wines <elijahwines84@yahoo.com>
2020-04-15 14:43:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,22 @@ class Student
     end
   end
 
+  def self.create(:name, :grade) 
+    student = Student.new(name, grade)
+    student.save 
+    student 
+  end
 
+  def self.new_from_db(row)
+      new_student = self.new 
+      new_student.id = row[0]
+      new_student.name = row[1]
+      new_student.grade = row[2]
+      new_student
+  end
+
+  
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 

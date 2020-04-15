90fa69baaa8f9f07f945bae96ff393f71a1e6f73
Elijah wines <elijahwines84@yahoo.com>
2020-04-15 14:50:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
-  end 
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
